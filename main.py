4e1432da80b64ff0b3916b6047d987a286ab9800
Endru <88160476+andrik27@users.noreply.github.com>
2024-10-18 23:36:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-n = str(input("Введите строку: "))
-print(n.replace("а","о"))
+words = str(input("Введите строку: ")) #Запрос строки от пользователя
+words = words.lower() #Нижний регистр
+for i in range(len(words)):  #Цикл
+    if words[i]=="е": #Цикл
+        print(i) #Вывод на экран
